b22f197216533408ac7f2014c1d9d360cbfd089a
Timothy Crundall <tim.crundall@gmail.com>
2024-03-30 22:09:08 +0100

    Refactor type definition to reuse definition methods

--- lua/omnisharp_extended.lua
+++ lua/omnisharp_extended.lua
@@ -1,5 +1,4 @@
 local m_definition = require("definition")
-local m_type_definition = require("type_definition")
 local m_references = require("references")
 local m_implementation = require("implementation")
 local o_utils = require("omnisharp_utils")
@@ -11,13 +10,13 @@ M.handler = m_definition.handler
 M.lsp_definitions = m_definition.omnisharp_command
 M.telescope_lsp_definitions = m_definition.telescope_command
 
-M.lsp_definition = m_definition.omnisharp_command
-M.telescope_lsp_definition = m_definition.telescope_command
-M.definition_handler = m_definition.handler
+M.lsp_definition = m_definition.definition.omnisharp_command
+M.telescope_lsp_definition = m_definition.definition.telescope_command
+M.definition_handler = m_definition.definition.handler
 
-M.lsp_type_definition = m_type_definition.omnisharp_command
-M.telescope_lsp_type_definition = m_type_definition.telescope_command
-M.type_definition_handler = m_type_definition.handler
+M.lsp_type_definition = m_definition.typeDefinition.omnisharp_command
+M.telescope_lsp_type_definition = m_definition.typeDefinition.telescope_command
+M.type_definition_handler = m_definition.typeDefinition.handler
 
 M.lsp_references = m_references.omnisharp_command
 M.telescope_lsp_references = m_references.telescope_command
